2ffc50c5afcd911da7ecdebbdd4bc64834f188bc
Jules0809 <debeejules952@gmail.com>
2025-10-17 15:06:20 +0200

    Resolution

--- client.c
+++ client.c
@@ -1,21 +1,3 @@
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-=======
-/* client.c
- * 
- * Client CLI simple pour communiquer avec server.c
- *
- * Usage:
- *    ./client
- * ou ./client <host> <port>
- * Commandes dispo : IDENT, sendTicket, etc.
- *
- * 
- */
-
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 #define _POSIX_C_SOURCE 200809L
 #include <stdio.h>
 #include <stdlib.h>
